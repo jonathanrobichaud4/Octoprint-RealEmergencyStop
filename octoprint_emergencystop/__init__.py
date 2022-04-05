c6ce70a0e48f6ccc83cd249a8d878cdae43f4ead
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2022-04-05 16:40:56 -0300

    version ffs

--- octoprint_emergencystop/__init__.py
+++ octoprint_emergencystop/__init__.py
@@ -190,7 +190,7 @@ class Emergency_stopPlugin(octoprint.plugin.StartupPlugin,
 __plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
 
 __plugin_name__ = "Emergency Stop"
-__plugin_version__ = "0.1.13"
+__plugin_version__ = "0.1.14"
 
 def __plugin_check__():
     try:
