9fb7a7c064de0576c036b72354d127113f057b6e
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2022-04-07 09:41:36 -0300

    css fix

--- octoprint_emergencystop/__init__.py
+++ octoprint_emergencystop/__init__.py
@@ -197,7 +197,7 @@ class Emergency_stopPlugin(octoprint.plugin.StartupPlugin,
 __plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
 
 __plugin_name__ = "Emergency Stop"
-__plugin_version__ = "0.1.45"
+__plugin_version__ = "0.1.46"
 
 def __plugin_check__():
     try:
